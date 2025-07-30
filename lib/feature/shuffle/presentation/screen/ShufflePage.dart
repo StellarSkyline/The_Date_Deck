f869392a59a33ea7e81ad8da5077fd8384a8cbf4
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-07-29 21:30:04 -0500

    Updated Shuffle Page
    Added Category Type Slider selector

--- lib/feature/shuffle/presentation/screen/ShufflePage.dart
+++ lib/feature/shuffle/presentation/screen/ShufflePage.dart
@@ -1,16 +1,26 @@
+import 'package:date_deck/feature/shuffle/presentation/components/HorizontalList.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ShufflePage extends StatelessWidget {
+class ShufflePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ShufflePageState();
+
+}
+
+class _ShufflePageState extends State<ShufflePage>{
+
     @override
     Widget build(BuildContext context) {
         return Column(
-            children:[
-                Text("Shuffle Page")
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+                Container(height:40, child: HorizontalList(onPressed: (index)=> {})),
+                SizedBox(height:300, width:300, child: Placeholder())
             ]
-
-
 
         );
     }
+
 }
+
