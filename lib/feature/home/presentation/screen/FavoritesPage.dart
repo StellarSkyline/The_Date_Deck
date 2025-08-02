47759505b83b512e711c3b6086ed49a798f40c12
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-08-02 12:00:09 -0500

    Update
    Added Shuffle page list on Favorites Page

--- lib/feature/home/presentation/screen/FavoritesPage.dart
+++ lib/feature/home/presentation/screen/FavoritesPage.dart
@@ -1,18 +1,59 @@
-import 'package:date_deck/feature/home/presentation/components/CardComponent.dart';
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../domain/HomeViewModel.dart';
+import '../components/CardComponent.dart';
 
-import '../../data/model/suit.dart';
+class FavoritesPage extends StatefulWidget{
+    @override
+    State<StatefulWidget> createState() => _FavoritesPageState();
 
+}
 
-class FavoritesPage extends StatelessWidget{
+class _FavoritesPageState extends State<FavoritesPage>{
+
+    //Widget State
+    var streamController = StreamController();
 
     @override
     Widget build(BuildContext context) {
-        return CardComponent(
-            name: "test",
-            suit: Suit.Diamond,
-            onPress: (index) => {}
+        final vm = context.watch<HomeViewModel>();
+        setState(() {
+                streamController = vm.streamController;
+            }
+        );
+        return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                SizedBox(height: 8),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: StreamBuilder(
+                            stream: streamController.stream,
+                            initialData: vm.initialDates,
+                            builder: (_, asyncSnapshot) {
+                                final dates = asyncSnapshot.data ?? List.empty();
+                                return ListView.builder(
+                                    itemCount: dates.length,
+                                    //Add Item Count here
+                                    itemBuilder: (context, index) {
+                                        return CardComponent(
+                                            name: dates[index].fullDescription,
+                                            suit: dates[index].suit,
+                                            onPress: (index) => {});
+                                    }
+                                );
+                            }
+                        )
+                    )
+                ),
+                SizedBox(height: 8)
+            ]
         );
     }
+
 }
