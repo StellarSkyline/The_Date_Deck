21d37b3206688f81b8cbce0cca7dada6b01a5f34
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-08-07 10:36:04 -0500

    Update FavoritesPage.dart

--- lib/feature/home/presentation/screen/FavoritesPage.dart
+++ lib/feature/home/presentation/screen/FavoritesPage.dart
@@ -51,7 +51,7 @@ class _FavoritesPageState extends State<FavoritesPage>{
                                                 name: dates[index].fullDescription,
                                                 suit: dates[index].suit,
                                                 favorite: dates[index].favorite,
-                                                categoryName: dates[index].category.toString(),
+                                                categoryName: dates[index].category.displayName,
                                                 onPress: (value) => {
                                                     dates[index].favorite ? dates[index].favorite = false : dates[index].favorite = true,
                                                     vm.updateDate(dates[index]),
