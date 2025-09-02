9dd3b1c73431a8cd07414ad06fd7eac606fe31fe
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-09-02 11:17:44 -0500

    Update ShufflePage.dart
    Added swipe functionality

--- lib/feature/home/presentation/screen/ShufflePage.dart
+++ lib/feature/home/presentation/screen/ShufflePage.dart
@@ -11,114 +11,104 @@ import '../components/CardComponent.dart';
 import '../components/EmptyHandler.dart';
 
 class ShufflePage extends StatefulWidget {
-    @override
-    State<StatefulWidget> createState() => _ShufflePageState();
+  @override
+  State<StatefulWidget> createState() => _ShufflePageState();
 }
 
 class _ShufflePageState extends State<ShufflePage> {
-    var streamController = StreamController();
-    var databaseState = true;
+  var streamController = StreamController();
+  var databaseState = true;
 
-    @override
-    Widget build(BuildContext context) {
-        final vm = context.watch<HomeViewModel>();
-        final controller = vm.controller;
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<HomeViewModel>();
+    final controller = vm.controller;
 
-        setState(() {
-                streamController = vm.streamControllerShuffle;
-            }
-        );
+    setState(() {
+      streamController = vm.streamControllerShuffle;
+    });
 
-        return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                SizedBox(height: 8),
-                Container(height: 42, child: HorizontalList(onPressed: (index) =>
-                        {
-                            vm.setCategoryIndex(index),
-                            vm.changeStreamDataCategory(Category.values[vm.categoryIndex]),
-                            vm.setInitialShuffleDate(Category.values[vm.categoryIndex])
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 8),
+        Container(
+          height: 42,
+          child: HorizontalList(
+            onPressed: (index) => {
+              vm.setCategoryIndex(index),
+              vm.changeStreamDataCategory(Category.values[vm.categoryIndex]),
+              vm.setInitialShuffleDate(Category.values[vm.categoryIndex]),
+            },
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: StreamBuilder(
+              stream: streamController.stream,
+              initialData: vm.initialShuffleDate,
+              builder: (_, asyncSnapshot) {
+                final dates = asyncSnapshot.data ?? List.empty();
+                if (dates.isEmpty) {
+                  return EmptyHandler(textTitle: "Please Shuffle Cards");
+                } else {
+                  return Flexible(
+                    child: CardSwiper(
+                      controller: controller,
+                      cardsCount: dates.length,
+                      allowedSwipeDirection: AllowedSwipeDirection.symmetric(horizontal:true, vertical:false),
+                      onSwipe: (pIndex, cIndex, direction) {
+                        switch (direction) {
+                          case CardSwiperDirection.right:
+                            dates[pIndex].favorite ? dates[pIndex].favorite = false : dates[pIndex].favorite = true;
+                            vm.updateDate(dates[pIndex]);
+                            vm.changeStreamDataFavorites();
+                          default:
+                            debugPrint(
+                              'The card $pIndex was swiped to the ${direction.name}. Now the card $cIndex is on top',
+                            );
                         }
-                    )),
-                Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: StreamBuilder(
-                            stream: streamController.stream,
-                            initialData: vm.initialShuffleDate,
-                            builder: (_, asyncSnapshot) {
-                                final dates = asyncSnapshot.data ?? List.empty();
-                                if (dates.isEmpty) {
-                                    return EmptyHandler(textTitle: "Please Shuffle Cards");
-                                }
-                                else {
-                                    return Flexible(
-                                      child: CardSwiper(
-                                        controller: controller,
-                                        cardsCount: dates.length,
-                                        cardBuilder: (context, index, horizontalThresholdPercentage,
-                                        verticalThresholdPercentage,) {
-                                          return CardComponent(
-                                              name: dates[index].fullDescription,
-                                              suit: dates[index].suit,
-                                              favorite: dates[index].favorite,
-                                              categoryName: dates[index].category.displayName,
-                                              onPress: (value) => {
-                                                dates[index].favorite ? dates[index].favorite = false : dates[index].favorite = true,
-                                                vm.updateDate(dates[index]),
-                                                vm.changeStreamDataFavorites(),
-                                                vm.setInitialShuffleDate(Category.values[vm.categoryIndex])
-                                              }
-                                          );
-                                        }
-                                      )
-                                    );
-                                }
-                            }
-                        )
-                    )
-                ),
-                Center(
-                    child: SizedBox(
-                        width: 228,
-                        height: 55,
-                        child: ElevatedButton(
-                            onPressed: () => {
-                                vm.changeStreamDataCategory(Category.values[vm.categoryIndex])
-                            },
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xFFE06F7C),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))
-                            ),
-                            child: Text("Shuffle Deck",
-                                style: TextStyle(color: Colors.white, fontSize: 16))
-                        )
-                    )
-                ),
-                SizedBox(height: 8)
-            ]
-        );
-    }
+                        return true;
+                      },
+                      cardBuilder: (context, index, horizontalThresholdPercentage, verticalThresholdPercentage) {
+                        return CardComponent(
+                          name: dates[index].fullDescription,
+                          suit: dates[index].suit,
+                          favorite: dates[index].favorite,
+                          categoryName: dates[index].category.displayName,
+                          onPress: (value) => {
+                            dates[index].favorite ? dates[index].favorite = false : dates[index].favorite = true,
+                            vm.updateDate(dates[index]),
+                            vm.changeStreamDataFavorites(),
+                            vm.setInitialShuffleDate(Category.values[vm.categoryIndex]),
+                          },
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ),
+        Center(
+          child: SizedBox(
+            width: 228,
+            height: 55,
+            child: ElevatedButton(
+              onPressed: () => {vm.changeStreamDataCategory(Category.values[vm.categoryIndex])},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFE06F7C),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+              child: Text("Shuffle Deck", style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+          ),
+        ),
+        SizedBox(height: 8),
+      ],
+    );
+  }
 }
-
-/*
-ListView.builder(
-                                        itemCount: 4,
-                                        itemBuilder: (context, index) {
-                                            return CardComponent(
-                                                name: dates[index].fullDescription,
-                                                suit: dates[index].suit,
-                                                favorite: dates[index].favorite,
-                                                categoryName: dates[index].category.displayName,
-                                                onPress: (value) => {
-                                                    dates[index].favorite ? dates[index].favorite = false : dates[index].favorite = true,
-                                                    vm.updateDate(dates[index]),
-                                                    vm.changeStreamDataFavorites(),
-                                                    vm.setInitialShuffleDate(Category.values[vm.categoryIndex])
-                                                }
-                                            );
-                                        }
-                                    );
- */
