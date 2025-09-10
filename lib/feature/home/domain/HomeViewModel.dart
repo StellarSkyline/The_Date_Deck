10ee8f67e7be9c1e77700b9356996f5c4b4f782a
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-09-09 23:03:12 -0500

    Update
    Revamped Logic for Loading Card Suit Graphics

--- lib/feature/home/domain/HomeViewModel.dart
+++ lib/feature/home/domain/HomeViewModel.dart
@@ -3,99 +3,132 @@ import 'dart:async';
 import 'package:date_deck/feature/home/data/model/date.dart';
 import 'package:date_deck/feature/home/data/repo/HomeRepository.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../feature/home/core/database/date_dao.dart';
 import '../data/model/category.dart';
 
 class HomeViewModel extends ChangeNotifier {
-    //Repo
-    final _HomeRepo = HomeRepository();
+  //Repo
+  final _HomeRepo = HomeRepository();
 
-    //State
-    late DateDao _dao;
-    DateDao get dao => _dao;
+  //State
+  late DateDao _dao;
 
-    final CardSwiperController _controller = CardSwiperController();
-    CardSwiperController get controller => _controller;
+  DateDao get dao => _dao;
 
+  final CardSwiperController _controller = CardSwiperController();
 
+  CardSwiperController get controller => _controller;
 
-    int _categoryIndex = 0;
-    int get categoryIndex => _categoryIndex;
+  int _categoryIndex = 0;
 
-    List<Date> _initialShuffleDate = List.empty();
-    List<Date> get initialShuffleDate => _initialShuffleDate;
+  int get categoryIndex => _categoryIndex;
 
-    List<Date> _initialFavoritesDate = List.empty();
-    List<Date> get initialFavoritesDate => _initialFavoritesDate;
+  List<Date> _initialShuffleDate = List.empty();
 
-    final StreamController<List<Date>> _streamControllerShuffle = StreamController<List<Date>>.broadcast();
-    StreamController<List<Date>> get streamControllerShuffle => _streamControllerShuffle;
+  List<Date> get initialShuffleDate => _initialShuffleDate;
 
-    final StreamController<List<Date>> _streamControllerFavorites = StreamController<List<Date>>.broadcast();
-    StreamController<List<Date>> get streamControllerFavorites => _streamControllerFavorites;
+  List<Date> _initialFavoritesDate = List.empty();
 
-    //Constructor
-    HomeViewModel() {
-        init();
-    }
+  List<Date> get initialFavoritesDate => _initialFavoritesDate;
 
-    //Methods
-    Future<void> init() async {
-        _dao = await _HomeRepo.buildDatabase();
-        initShuffle();
-        initFavorites();
-        notifyListeners();
-    }
+  final StreamController<List<Date>> _streamControllerShuffle = StreamController<List<Date>>.broadcast();
 
-    Future<void> initShuffle() async {
-        _initialShuffleDate = await dao.getAllByCategoryAsStream(Category.Active);
-        _initialShuffleDate.shuffle();
-    }
+  StreamController<List<Date>> get streamControllerShuffle => _streamControllerShuffle;
 
-    void setCategoryIndex(int index) {
-        _categoryIndex = index;
-        notifyListeners();
-    }
+  final StreamController<List<Date>> _streamControllerFavorites = StreamController<List<Date>>.broadcast();
 
-    Future<void> setInitialShuffleDate(Category category) async {
-        _initialShuffleDate = await dao.getAllByCategoryAsStream(category);
-        notifyListeners();
-    }
+  StreamController<List<Date>> get streamControllerFavorites => _streamControllerFavorites;
 
-    Future<void> initFavorites() async {
-        _initialFavoritesDate = await dao.getFavorites();
-    }
+  //Graphics Logic
+  //Replace img_default with illustrations
+  final _graphics = [
+    Image.asset('assets/images/img_ace.png', fit: BoxFit.cover),
+    SvgPicture.asset('assets/images/img_2.svg', fit: BoxFit.contain),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+    SvgPicture.asset('assets/images/img_default.svg'),
+  ];
+  get graphics => _graphics;
 
-    void updateDate(Date date) {
-        dao.updateDate(date);
-        notifyListeners();
-    }
+  final _cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  get cardNumbers => _cardNumbers;
 
-    void changeStreamDataCategory(Category category) {
-        dao.getAllByCategoryAsStream(category).then((data) {
-                data.shuffle();
-                _streamControllerShuffle.sink.add(data);
-                _initialShuffleDate = data;
-            }
-        );
-        notifyListeners();
-    }
+  final _suitGraphics = [
+    {'suit': SvgPicture.asset('assets/icons/icn_club.svg', fit: BoxFit.fill), 'color': Colors.black},
+    {'suit': SvgPicture.asset('assets/icons/icn_spade.svg', fit: BoxFit.fill), 'color': Colors.black},
+    {'suit':SvgPicture.asset('assets/icons/icn_heart.svg', fit: BoxFit.fill), 'color':Color(0xFFE06F7C)},
+    {'suit':SvgPicture.asset('assets/icons/icn_diamond.svg', fit: BoxFit.fill), 'color':Color(0xFFE06F7C)}
+  ];
+  get suitGraphics => _suitGraphics;
 
-    void changeStreamDataFavorites() {
-        dao.getAllByCategoryAsStream(Category.values[_categoryIndex]).then((data) {
-                data.shuffle();
-                _initialShuffleDate = data;
-            }
-        );
+  //Constructor
+  HomeViewModel() {
+    init();
+  }
 
-        dao.getFavorites().then((data) {
-                _streamControllerFavorites.sink.add(data);
-                _initialFavoritesDate = data;
-            }
-        );
-        notifyListeners();
-    }
+  //Methods
+  Future<void> init() async {
+    _dao = await _HomeRepo.buildDatabase();
+    initShuffle();
+    initFavorites();
+    notifyListeners();
+  }
 
+  Future<void> initShuffle() async {
+    _initialShuffleDate = await dao.getAllByCategoryAsStream(Category.Active);
+    _initialShuffleDate.shuffle();
+  }
+
+  void setCategoryIndex(int index) {
+    _categoryIndex = index;
+    notifyListeners();
+  }
+
+  Future<void> setInitialShuffleDate(Category category) async {
+    _initialShuffleDate = await dao.getAllByCategoryAsStream(category);
+    notifyListeners();
+  }
+
+  Future<void> initFavorites() async {
+    _initialFavoritesDate = await dao.getFavorites();
+  }
+
+  void updateDate(Date date) {
+    dao.updateDate(date);
+    notifyListeners();
+  }
+
+  void changeStreamDataCategory(Category category) {
+    dao.getAllByCategoryAsStream(category).then((data) {
+      data.shuffle();
+      _streamControllerShuffle.sink.add(data);
+      _initialShuffleDate = data;
+    });
+    notifyListeners();
+  }
+
+  void changeStreamDataFavorites() {
+    dao.getAllByCategoryAsStream(Category.values[_categoryIndex]).then((data) {
+      data.shuffle();
+      _initialShuffleDate = data;
+    });
+
+    dao.getFavorites().then((data) {
+      _streamControllerFavorites.sink.add(data);
+      _initialFavoritesDate = data;
+    });
+    notifyListeners();
+  }
 }
--- lib/feature/home/presentation/components/CardComponent.dart
+++ lib/feature/home/presentation/components/CardComponent.dart
@@ -1,151 +1,97 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 import 'dart:math' as Math;
 
 import '../../data/model/suit.dart';
+import '../../domain/HomeViewModel.dart';
 
 class CardComponent extends StatelessWidget {
+  final String name;
+  final Suit suit;
+  final bool favorite;
+  final ValueChanged onPress;
+  final String categoryName;
+  final int effortValue;
 
-    final String name;
-    final Suit suit;
-    final bool favorite;
-    final ValueChanged onPress;
-    final String categoryName;
-    final int effortValue;
+  CardComponent({required this.name, required this.suit, required this.favorite, required this.onPress, required this.categoryName, required this.effortValue});
 
-    CardComponent({required this.name, required this.suit, required this.favorite, required this.onPress, required this.categoryName, required this.effortValue});
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<HomeViewModel>();
 
-    @override
-    Widget build(BuildContext context) {
-        Widget cardSuit = SvgPicture.asset('assets/icons/icn_club.svg');
-        Color textColor = Colors.black;
-        List<String> cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-        String cardValue = cardNumbers[effortValue-1];
-
-        switch (suit) {
-            case Suit.Club:
-                cardSuit = SvgPicture.asset('assets/icons/icn_club.svg',height:20,width:20);
-                textColor = Colors.black;
-            case Suit.Spade:
-                cardSuit = SvgPicture.asset('assets/icons/icn_spade.svg',height:20,width:20);
-                textColor = Colors.black;
-            case Suit.Heart:
-                cardSuit = SvgPicture.asset('assets/icons/icn_heart.svg',height:20,width:20);
-                textColor = Color(0xFFE06F7C);
-            case Suit.Diamond:
-                cardSuit = SvgPicture.asset('assets/icons/icn_diamond.svg',height:20,width:20);
-                textColor = Color(0xFFE06F7C);
-        }
-
-        return SizedBox(
-            width: 483,
-            height: 609,
-            child: Card(
-              elevation: 8,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                color: Color(0xFFFBF8F0),
-                child: Padding(
-                    padding: EdgeInsetsGeometry.all(8.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                    Column(
-                                        children: [
-                                            Text(cardValue,
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    color: textColor
-                                                )
-                                            ),
-                                            cardSuit
-                                        ]
-                                    )
-                                ]
-                            ),
-                            Text(
-                                "Category: $categoryName",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 17,
-                                    color: Colors.black
-
-                                )
-                            ),
-                            Expanded(
-                                child: Image.asset(
-                                    'assets/images/img_test.png',
-                                  fit: BoxFit.cover
-                                )
-                            ),
-                          Padding(
-                              padding: EdgeInsetsGeometry.all(8.0),
-                              child: Text(
-                                  name,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black
-                                  )
-                              )
+    return SizedBox(
+      width: 483,
+      height: 609,
+      child: Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Color(0xFFFBF8F0),
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Text(vm.cardNumbers[effortValue - 1], style: TextStyle(fontSize: 20, color: vm.suitGraphics[suit.index]['color'])),
+                      SizedBox(width: 20, height: 20, child:vm.suitGraphics[suit.index]['suit']),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                "Category: $categoryName",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 17, color: Colors.black),
+              ),
+              Expanded(child: vm.graphics[effortValue - 1]),
+              Padding(
+                padding: EdgeInsetsGeometry.all(8.0),
+                child: Text(
+                  name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: SizedBox(
+                        width: 150,
+                        height: 35,
+                        child: ElevatedButton(
+                          onPressed: () => {onPress(0)},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            backgroundColor: (favorite ? Color(0xFFE06F7C) : Color(0xFF5E4E81)),
                           ),
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                    Expanded(
-                                        child: Center(
-                                            child: SizedBox(
-                                                width: 150,
-                                                height: 35,
-                                                child: ElevatedButton(
-                                                    onPressed: () => {
-                                                        onPress(0)
-                                                    },
-                                                    style: ElevatedButton.styleFrom(
-                                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                                                        backgroundColor: (favorite ? Color(0xFFE06F7C) : Color(0xFF5E4E81))
-                                                    ),
-                                                    child: Text(favorite ? "Remove" : "Favorite",
-                                                        style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontSize: 15.0
-                                                        )
-                                                    )
-                                                )
-                                            )
-                                        )
-                                    ),
-                                    Column(
-                                        children: [
-
-                                            Transform.rotate(
-                                                angle: Math.pi,
-                                                child: cardSuit
-                                            ),
-                                            Transform.rotate(
-                                                angle: Math.pi,
-                                                child: Text(cardValue,
-                                                    style: TextStyle(
-                                                        fontSize: 20,
-                                                        color: textColor
-                                                    )
-                                                )
-                                            )
-                                        ]
-                                    )
-
-                                ]
-                            )
-
-                        ]
-                    )
-                )
-            )
-        );
-    }
-
+                          child: Text(favorite ? "Remove" : "Favorite", style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Transform.rotate(angle: Math.pi, child: SizedBox(width: 20, height: 20, child:vm.suitGraphics[suit.index]['suit'])),
+                      Transform.rotate(
+                        angle: Math.pi,
+                        child: Text(vm.cardNumbers[effortValue - 1], style: TextStyle(fontSize: 20, color: vm.suitGraphics[suit.index]['color'])),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
