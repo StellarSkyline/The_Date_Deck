e1abe8ae0b56703fdbe3ce4fc847ded53cb943fd
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-09-12 11:14:31 -0500

    Update
    Updated Splash Screen image

--- lib/feature/home/presentation/screen/SplashPage.dart
+++ lib/feature/home/presentation/screen/SplashPage.dart
@@ -6,51 +6,41 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'HomeScreen.dart';
 
 class SplashPage extends StatefulWidget {
-    @override
-    State<StatefulWidget> createState() => _SplashPageState();
+  @override
+  State<StatefulWidget> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
 
-    @override
-    void initState() {
-        super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
 
-        Timer(Duration(seconds: 3), () {
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-            }
-        );
-    }
-
-    @override
-    Widget build(BuildContext context) {
-
-        return Container(
-            color: Color(0xFFF4F4F4),
-            child: SafeArea(
-                child: Scaffold(
-                    backgroundColor: Color(0xFFF4F4F4),
-                    body: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                            SvgPicture.asset('assets/images/img_main.svg',
-                                width: 350,
-                                height: 500
-                            ),
-                            Text("The Random Date Idea Generator",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xFF5E4E81)
-                                )
-                            )
-                        ]
-                    )
-                )
-
-            )
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFF4F4F4),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(0xFFF4F4F4),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset('assets/images/img_splash.png', width: 350, height: 500, fit: BoxFit.contain),
+              Text(
+                "The Random Date Idea Generator",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, color: Color(0xFF5E4E81)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
-
