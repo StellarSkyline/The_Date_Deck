1cd78f8741e108d8b31a9e4eacc1a1a6bb8426da
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-10-06 14:31:37 -0500

    Added final illustrations

--- lib/feature/home/domain/HomeViewModel.dart
+++ lib/feature/home/domain/HomeViewModel.dart
@@ -46,29 +46,32 @@ class HomeViewModel extends ChangeNotifier {
   final _graphics = [
     Image.asset('assets/images/img_1.png', fit: BoxFit.contain),
     Image.asset('assets/images/img_2.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_3.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_4.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_5.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_6.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_7.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_8.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_9.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_10.png', fit: BoxFit.contain),
-     Image.asset('assets/images/img_default.png'),
-     Image.asset('assets/images/img_default.png'),
-     Image.asset('assets/images/img_default.png'),
+    Image.asset('assets/images/img_3.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_4.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_5.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_6.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_7.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_8.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_9.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_10.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_11.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_12.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_13.png', fit: BoxFit.contain),
   ];
+
   get graphics => _graphics;
 
   final _cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
   get cardNumbers => _cardNumbers;
 
   final _suitGraphics = [
     {'suit': SvgPicture.asset('assets/icons/icn_club.svg', fit: BoxFit.fill), 'color': Colors.black},
     {'suit': SvgPicture.asset('assets/icons/icn_spade.svg', fit: BoxFit.fill), 'color': Colors.black},
-    {'suit': SvgPicture.asset('assets/icons/icn_heart.svg', fit: BoxFit.fill), 'color':Color(0xFFE06F7C)},
-    {'suit': SvgPicture.asset('assets/icons/icn_diamond.svg', fit: BoxFit.fill), 'color':Color(0xFFE06F7C)}
+    {'suit': SvgPicture.asset('assets/icons/icn_heart.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
+    {'suit': SvgPicture.asset('assets/icons/icn_diamond.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
   ];
+
   get suitGraphics => _suitGraphics;
 
   //Constructor
@@ -84,7 +87,7 @@ class HomeViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-   void initShuffle() async {
+  void initShuffle() async {
     _initialShuffleDate = await _dao.getAllByCategoryAsStream(Category.Active);
     _initialShuffleDate.shuffle();
   }
