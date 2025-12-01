151003a6b3742c2fa8d1e0e84cf614392d9e5f24
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-12-01 15:44:05 -0600

    Refactor

--- lib/feature/home/domain/HomeViewModel.dart
+++ lib/feature/home/domain/HomeViewModel.dart
@@ -4,7 +4,6 @@ import 'package:date_deck/feature/home/data/model/date.dart';
 import 'package:date_deck/feature/home/data/repo/HomeRepository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-import 'package:flutter_svg/svg.dart';
 
 import '../../../feature/home/core/database/date_dao.dart';
 
@@ -16,12 +15,17 @@ class HomeViewModel extends ChangeNotifier {
   late DateDao _dao;
 
   final CardSwiperController _controller = CardSwiperController();
+
   CardSwiperController get controller => _controller;
 
   int _categoryIndex = 0;
+
   int get categoryIndex => _categoryIndex;
+
   get graphics => _HomeRepo.graphicsList;
+
   get cardNumbers => _HomeRepo.cardNumber;
+
   get suitGraphics => _HomeRepo.suitList;
 
   //Constructor
@@ -50,31 +54,26 @@ class HomeViewModel extends ChangeNotifier {
   void insertDate(Date date) async {
     //Verify if the date exits
     final savedDate = await _dao.findById(date.id);
-    if(savedDate == null) _dao.insertDate(date);
+    if (savedDate == null) _dao.insertDate(date);
   }
 
   Future<List<Date>> getFavorites() {
     return _dao.getAllDates();
   }
 
-  Future<bool> checkDate(int id) async {
-    final savedDate = await _dao.findById(id);
-    if(savedDate == null) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   //Network Methods
   Future<List<Date>> getCategory(int index) {
-    switch(index) {
-      case 0: return _HomeRepo.getActiveDates();
-      case 1: return _HomeRepo.getCreativeDates();
-      case 2: return _HomeRepo.getGamesDates();
-      case 3: return _HomeRepo.getCookingDates();
-      default: return _HomeRepo.getActiveDates();
+    switch (index) {
+      case 0:
+        return _HomeRepo.getActiveDates();
+      case 1:
+        return _HomeRepo.getCreativeDates();
+      case 2:
+        return _HomeRepo.getGamesDates();
+      case 3:
+        return _HomeRepo.getCookingDates();
+      default:
+        return _HomeRepo.getActiveDates();
     }
-
   }
 }
