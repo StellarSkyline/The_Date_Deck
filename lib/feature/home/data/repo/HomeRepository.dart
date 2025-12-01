e706e43cee9079ec1e6254235992159572073aeb
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-12-01 10:22:23 -0600

    Update
    Minor Refactor

--- lib/feature/home/data/repo/HomeRepository.dart
+++ lib/feature/home/data/repo/HomeRepository.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 
 import '../../core/Constants.dart';
@@ -9,11 +11,10 @@ import '../../core/database/date_dao.dart';
 import '../model/date.dart';
 
 class HomeRepository {
-  //Database Initalization
+  //Database Initialization
   Future<DateDao> buildDatabase() async {
     WidgetsFlutterBinding.ensureInitialized();
     final database = await $FloorAppDatabase.databaseBuilder('flutter_database.db').build();
-
     return database.dateDao;
   }
 
@@ -37,4 +38,32 @@ class HomeRepository {
   Future<List<Date>> getCookingDates() => getDates('cooking');
   Future<List<Date>> getCreativeDates() => getDates('creative');
   Future<List<Date>> getGamesDates() => getDates('games');
+
+
+  //Graphics Assets
+  final graphicsList = [
+    Image.asset('assets/images/img_1.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_2.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_3.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_4.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_5.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_6.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_7.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_8.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_9.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_10.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_11.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_12.png', fit: BoxFit.contain),
+    Image.asset('assets/images/img_13.png', fit: BoxFit.contain),
+  ];
+
+  final suitList = [
+    {'suit': SvgPicture.asset('assets/icons/icn_club.svg', fit: BoxFit.fill), 'color': Colors.black},
+    {'suit': SvgPicture.asset('assets/icons/icn_spade.svg', fit: BoxFit.fill), 'color': Colors.black},
+    {'suit': SvgPicture.asset('assets/icons/icn_heart.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
+    {'suit': SvgPicture.asset('assets/icons/icn_diamond.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
+  ];
+
+  final cardNumber = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
 }
--- lib/feature/home/domain/HomeViewModel.dart
+++ lib/feature/home/domain/HomeViewModel.dart
@@ -20,36 +20,10 @@ class HomeViewModel extends ChangeNotifier {
 
   int _categoryIndex = 0;
   int get categoryIndex => _categoryIndex;
+  get graphics => _HomeRepo.graphicsList;
+  get cardNumbers => _HomeRepo.cardNumber;
+  get suitGraphics => _HomeRepo.suitList;
 
-  get graphics => _graphics;
-  final _cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  get cardNumbers => _cardNumbers;
-  get suitGraphics => _suitGraphics;
-
-  //Graphics Logic
-  final _graphics = [
-    Image.asset('assets/images/img_1.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_2.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_3.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_4.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_5.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_6.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_7.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_8.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_9.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_10.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_11.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_12.png', fit: BoxFit.contain),
-    Image.asset('assets/images/img_13.png', fit: BoxFit.contain),
-  ];
-
-  final _suitGraphics = [
-    {'suit': SvgPicture.asset('assets/icons/icn_club.svg', fit: BoxFit.fill), 'color': Colors.black},
-    {'suit': SvgPicture.asset('assets/icons/icn_spade.svg', fit: BoxFit.fill), 'color': Colors.black},
-    {'suit': SvgPicture.asset('assets/icons/icn_heart.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
-    {'suit': SvgPicture.asset('assets/icons/icn_diamond.svg', fit: BoxFit.fill), 'color': Color(0xFFE06F7C)},
-  ];
-  
   //Constructor
   HomeViewModel() {
     init();
