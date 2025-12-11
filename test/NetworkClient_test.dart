dfd79a6aa4bca1c509ac9cea7596587f963a5d90
Seth Osric Torralba <sethosrictorralba@gmail.com>
2025-12-11 11:06:52 -0600

    Update
    Minor Refactors

--- test/NetworkClient_test.dart
+++ test/NetworkClient_test.dart
@@ -1,5 +1,4 @@
 import 'package:date_deck/feature/home/core/network/NetworkClient.dart';
-import 'package:date_deck/feature/home/data/model/date.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
@@ -14,10 +13,9 @@ void main() {
       '{"0:{"Category": Active", "Effort Value": 7, "Name": "TEST TEST", "Suit": Club, "id": 2271"}"}';
 
   group('Network Client Test', () {
-
     test('get Date Success', () async {
       final client = MockHttpClient();
-      when(client.get(Uri.https(baseUrl, '/active.json')))
+      when(client.get(Uri.https(baseUrl,'/active.json')))
           .thenAnswer((_) async => http.Response(mockData, 200));
 
       expect(await NetworkClient(client: client).get('active'), isA<http.Response>());
