3230828b88d137fb48e8a90bd2b8a2f16ae71f68
Seth <sethosrictorralba@gmail.com>
2026-04-24 21:57:30 -0500

    Corrects JSON key for effort value in Date model
    Adjusts the key from 'Effort Value' to 'EffortValue' to properly map the incoming JSON data to the Date model.

--- lib/feature/home/data/model/date.dart
+++ lib/feature/home/data/model/date.dart
@@ -26,7 +26,7 @@ class Date {
       name: json['Name'] as String,
       category: Category.values.byName(json['Category']),
       suit: Suit.values.byName(json['Suit']),
-      effortValue: json['Effort Value'],
+      effortValue: json['EffortValue'],
     );
   }
 }
