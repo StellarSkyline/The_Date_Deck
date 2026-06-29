954a0bf8ddfa6d83f03c07a93d8531329ae5bfd5
Seth <sethosrictorralba@gmail.com>
2026-06-28 20:10:32 -0500

    chore(package name): Updated package name to com.topazskyline.datedeck
    Signed-off-by: Seth <sethosrictorralba@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,5 @@
-import java.util.Properties
 import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     id("com.android.application")
@@ -19,7 +19,7 @@ if (keystorePropertiesFile.exists()) {
 
 
 android {
-    namespace = "com.TopazSkyline.date_deck"
+    namespace = "com.topazskyline.datedeck"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.TopazSkyline.date_deck"
+        applicationId = "com.topazskyline.datedeck"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/TopazSkyline/date_deck/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.TopazSkyline.date_deck
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
